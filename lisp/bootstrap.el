17d7279fcc430b552cf2a9ce6f4724c2119c9dc7
sjkyspa <sjkyspa@gmail.com>
2020-03-07 21:04:44 +0800

    refactor: refactor the bootstrap

--- lisp/bootstrap.el
+++ lisp/bootstrap.el
@@ -4,13 +4,23 @@
 
 ;;; Code:
 
-(setq debug-on-error t)
+;;;; Public variables
+(defvar hexo-renderer-org-cachedir "./.cache"
+  "Cache directory to save generated result and Emacs packages to increase startup speed.")
 
-(let ((minver "24.4"))
-  (when (version< emacs-version minver)
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version< emacs-version "25.1")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+(defvar hexo-renderer-org-theme ""
+  "User's theme to use, it's recommand to use Emacs's builtin theme here.")
+
+(defvar hexo-renderer-org-common-block "#+OPTIONS: html-postamble:nil num:nil toc:nil ^:nil"
+  "Common `org-mode' settings in string, like #+OPTIONS: html-postamble:nil num:nil toc:nil ^:nil .")
+
+(defvar hexo-renderer-org-debug nil
+  "Enable debug message or not.")
+
+(defvar hexo-renderer-org-htmlize "false"
+  "Enable use Emacs's htmlize package to renderer code block or not.")
+
+(setq user-emacs-directory (concat (file-name-as-directory ".cache") "emacs.d")) 
 
 (defconst m/os
   (let ((os (symbol-name system-type)))
@@ -19,47 +29,34 @@
           ((or (string-prefix-p "ms" os) (string-prefix-p "windows" os)) 'windows))))
 
 (defconst m/root (file-name-directory (or load-file-name (buffer-file-name))))
-(defconst m/conf.d m/root)
+(add-to-list 'load-path user-emacs-directory)
 
+(defconst m/cache.d (file-name-as-directory ".cache"))
+
+(unless hexo-renderer-org-debug
+  (setq inhibit-message t))
 (setq gc-cons-threshold 100000000)
+;; Ignore all directory-local variables in `.dir-locals.el', whick make Emacs stucks there.
+(setq enable-dir-local-variables nil)
 
-(setq org-confirm-babel-evaluate nil)
 (setq make-backup-files nil)
-  ; stop to create the lock files
-  ; lock files is used to prevent concurrent edit of a file
+					; stop to create the lock files
+					; lock files is used to prevent concurrent edit of a file
 (setq create-lockfiles nil)
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((R . t)
-   (dot . t)
-   (ditaa . t)
-   (dot . t)
-   (emacs-lisp . t)
-   (gnuplot . t)
-   (haskell . nil)
-   (latex . t)
-   (ledger . t)
-   (ocaml . nil)
-   (octave . t)
-   (plantuml . t)
-   (python . t)
-   (ruby . t)
-   (screen . nil)
-   (shell . t)
-   (sql . t)
-   (sqlite . t)))
-
+;; Work-around for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+(when (version= "26.2" emacs-version)
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
 (require 'package)
 ;;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
 (let ((versioned-package-dir
        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
-                         m/root)))
+                         user-emacs-directory)))
   (setq package-user-dir versioned-package-dir))
 
 
@@ -74,10 +71,6 @@
       (setq package-archives '(("gnu"   . "https://elpa.emacs-china.org/gnu/")
 			       ("melpa" . "https://elpa.emacs-china.org/melpa/")
 			       ("org" . "https://elpa.emacs-china.org/org/"))))))
-;; Work-around for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-(when (version= "26.2" emacs-version)
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
-
 (package-initialize)
 (package-refresh-contents)
 
@@ -87,42 +80,7 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-(use-package auto-package-update
-  :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
 
-(add-to-list 'load-path m/root)
-
-;; Ignore all directory-local variables in `.dir-locals.el', whick make Emacs stucks there.
-(setq enable-dir-local-variables nil)
-;; Allow use #+BIND: in org-mode
-(setq org-export-allow-bind-keywords t)
-
-;;;; Public variables
-(defvar hexo-renderer-org-cachedir "./hexo-org-cache"
-  "Cache directory to save generated result and Emacs packages to increase startup speed.")
-
-(defvar hexo-renderer-org-user-config ""
-  "User's personal init.el to install extra packages or customize their own `org-mode' exporter.")
-
-(defvar hexo-renderer-org-theme ""
-  "User's theme to use, it's recommand to use Emacs's builtin theme here.")
-
-(defvar hexo-renderer-org-common-block "#+OPTIONS: html-postamble:nil num:nil toc:nil ^:nil"
-  "Common `org-mode' settings in string, like #+OPTIONS: html-postamble:nil num:nil toc:nil ^:nil .")
-
-(defvar hexo-renderer-org-htmlize "false"
-  "Enable use Emacs's htmlize package to renderer code block or not.")
-
-;;;; Private variables
-(defvar hexo-renderer-org--debug-file "./hexo-org-renderer.log"
-  "YOU SHOULD NOT SETUP THIS VARIABLE.")
-
-(defvar hexo-renderer-org--use-htmlize nil
-  "YOU SHOULD NOT SETUP THIS VARIABLE.
-This variable is keeped incase org-hexo not loaded.")
 
 (use-package exec-path-from-shell
   :config
@@ -138,7 +96,29 @@ This variable is keeped incase org-hexo not loaded.")
   (cnfonts-enable))
 
 (use-package org
-  :ensure org-plus-contrib)
+  :ensure org-plus-contrib
+  :config
+  ;; Allow use #+BIND: in org-mode
+  (setq org-confirm-babel-evaluate nil)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((R . t)
+     (dot . t)
+     (ditaa . t)
+     (emacs-lisp . t)
+     (gnuplot . t)
+     (haskell . nil)
+     (latex . t)
+     (ledger . t)
+     (ocaml . nil)
+     (octave . t)
+     (plantuml . t)
+     (python . t)
+     (ruby . t)
+     (screen . nil)
+     (shell . t)
+     (sql . t)
+     (sqlite . t))))
 
 (use-package ob-ditaa
   :after org
@@ -148,7 +128,7 @@ This variable is keeped incase org-hexo not loaded.")
 	       (file-exists-p org-ditaa-jar-path))
     (let ((jar-name "ditaa.jar")
 	  (url "https://github.com/stathissideris/ditaa/releases/download/v0.11.0/ditaa-0.11.0-standalone.jar"))
-      (setq org-ditaa-jar-path (expand-file-name jar-name m/conf.d))
+      (setq org-ditaa-jar-path (expand-file-name jar-name m/cache.d))
       (unless (file-exists-p org-ditaa-jar-path)
 	(url-copy-file url org-ditaa-jar-path)))))
 
@@ -160,7 +140,7 @@ This variable is keeped incase org-hexo not loaded.")
 	       (file-exists-p org-plantuml-jar-path))
     (let ((jar-name "plantuml.jar")
 	  (url "https://downloads.sourceforge.net/project/plantuml/1.2020.2/plantuml.1.2020.2.jar"))
-      (setq org-plantuml-jar-path (expand-file-name jar-name m/conf.d))
+      (setq org-plantuml-jar-path (expand-file-name jar-name m/cache.d))
       (unless (file-exists-p org-plantuml-jar-path)
 	(url-copy-file url org-plantuml-jar-path)))))
 
@@ -176,15 +156,8 @@ This variable is keeped incase org-hexo not loaded.")
 
 (defun hexo-org-renderer-oops (msg)
   "OOPS: something error, let's show the MSG and kill EMACS :(."
-  (use-package json)                       ; built-in
-  (let ((oops '(:success :json-false)))
-    (plist-put oops :message msg)
-    ;; Convert to JSON format and write to `*deebug-file*'
-    (with-temp-buffer
-      (insert (json-encode oops))
-      (write-region (point-min) (point-max) hexo-renderer-org--debug-file))
-    ;; bye-bye emacs
-    (kill-emacs)))
+    (princ msg)
+    (kill-emacs))
 
 ;; The emacs daemon SHOULD die when error occurs.
 (run-with-idle-timer
