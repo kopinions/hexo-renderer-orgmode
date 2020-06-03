1231648dfd9e6d6f0131434f8e9167644605fc9c
neo <sjkyspa@gmail.com>
2020-06-03 20:54:01 +0800

    feat: should able to run emacs with server mode when generate

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 'use strict';
 var renderer = require('./lib/renderer');
+const {server} = require('./lib/emacs.js')
 var {filter} = require('./lib/transformer');
-
+const emacsed = () => {
+    const process = require('process')
+    var server_mode = process.argv.indexOf('server') > 0 || process.argv.indexOf('s') > 0 || process.argv.indexOf('--watch') > 0 || process.argv.indexOf('-w') > 0;
+    if (server_mode || process.argv.indexOf('render') > 0 || process.argv.indexOf('generate') > 0 || process.argv.indexOf('g') > 0) {
+	return true
+    }
+    return false
+}
 hexo.on('ready', function() {
-    console.log("ready");
+    if (emacsed()) {
+	server.start(hexo)
+    }
 });
 // If use `hexo server`, the hexo will first enter `.on(exit)` event then start the server.
 hexo.on('exit', function(err) {
-    console.log("exit");
+    if (emacsed()) {
+	server.stop(hexo)
+    }
+
+    if (err) {
+	console.error(err)
+    }
 });
 
 hexo.extend.filter.register('before_post_render', filter);
