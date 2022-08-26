881636367adeaa11e6fe1456b59f122934e22b2f
neo <rising.lambda@gmail.com>
2022-08-26 16:39:43 +0800

    fix: remove auto-compile

--- lisp/bootstrap.el
+++ lisp/bootstrap.el
@@ -119,13 +119,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package auto-compile
-  :ensure t
-  :custom (load-prefer-newer t)
-  :config
-  (auto-compile-on-load-mode)
-  (auto-compile-on-save-mode))
-
 (use-package exec-path-from-shell
   :config
   (when (memq m/os '(macos linux))
